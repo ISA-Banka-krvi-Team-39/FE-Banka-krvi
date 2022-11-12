4b4ee939ff3690c136fd1129d1b8b50c08959e19
unknown <milic.ra208.2019@uns.ac.rs>
2022-11-12 17:54:37 +0100

    implemented center registration

--- src/shared-components/model/center/center.ts
+++ src/shared-components/model/center/center.ts
@@ -1,19 +1,17 @@
 import { Address } from "../shared/Address";
-import { PersonDTO } from "../shared/Person";
 
-export class Center {
-    address:Address;
-    name:string;
-    description:string;
-    avg_grade:string;
-    workingMedicalStaff:PersonDTO[];
+export class Center{
+    address: Address;
+    name: string;
+    description: string;
+    avg_grade: number;
+    
+    constructor(address: Address, name: string, description: string, avg_grade: number){
+        this.address = address;
+        this.name = name;
+        this.description = description;
+        this.avg_grade = avg_grade
 
-
-    constructor(address: Address,name:string,description:string,avg_grade:string,persons:PersonDTO[]) {
-      this.address = address;
-      this.name = name;
-      this.description = description;
-      this.avg_grade = avg_grade;
-      this.workingMedicalStaff = persons;
     }
-  }
+
+}
