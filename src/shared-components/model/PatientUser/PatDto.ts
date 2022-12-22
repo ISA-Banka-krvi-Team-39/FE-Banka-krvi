b57baf6f809f94dea4caa660918f23764c174487
mihajlo182savic <savic.ra182.2019@uns.ac.rs>
2022-12-22 20:25:28 +0100

    bugfix

---
+++ src/shared-components/model/PatientUser/PatDto.ts
@@ -0,0 +1,14 @@
+
+
+export class PatDto {
+    name:string;
+    surname:string;
+    date:string;
+
+    constructor(name:string,surname: string,date:string) {
+      this.name = name;
+      this.surname = surname;
+      this.date = date;
+      
+    }
+  }
